dfa2c3675e41a3199e34a55150668d5469508a59
Youssef Khalil <youssefkhalil@Youssefs-MacBook-Pro.local>
2020-05-26 02:55:03 -0400

    buildSimulation function to replace html script

--- js/spa.js
+++ js/spa.js
@@ -1,6 +1,7 @@
-// Tundra JS
+
 let app = {
     pages: [],
+    
     show: new Event('show'),
     init: () => {
       console.log("after app.init")
@@ -19,7 +20,7 @@ let app = {
         document.getElementById("diveinchiCharacter").addEventListener("click", app.nav);
         document.getElementById("homeButton").addEventListener("click", app.nav);
         document.getElementById("s1Button").addEventListener("click", app.nav);
-        document.getElementById("s2Button").addEventListener("click", app.nav);
+        document.getElementById("s2Button").addEventListener("click", app.buildSimulation);
         document.getElementById("s3Button").addEventListener("click", app.nav);
         document.getElementById("s4Button").addEventListener("click", app.nav);
         document.getElementById("s5Button").addEventListener("click", app.nav);
@@ -57,6 +58,50 @@ let app = {
       history.pushState({}, currentPage, `#${currentPage}`);
       document.getElementById(hash).dispatchEvent(app.show);
     },
+    buildSimulation: (simNumber) =>{
+      let body = document.getElementById("body");
+
+      //<section class="page">
+      let simPage = document.createElement("section");
+      simPage.classList.add("page");
+      body.appendChild(simPage);
+
+      //<div class="albumHead">Simulation_1: Testing</div>
+      let simHead = document.createElement("div");
+      simHead.classList.add("albumHead");
+      simHead.textContent("Simulation_"+simNumber+": Testing");
+      simPage.appendChild(simHead);
+
+      //<div class="albumContainer">
+      let simContainer = document.createElement("div");
+      simContainer.classList.add("albumContainer");
+      simPage.appendChild(simContainer);
+
+      //<div class="row">
+      let simRow = document.createElement("div");
+      simRow.classList.add("row");
+      simContainer.appendChild(simRow);
+
+      //<div class = "cover">
+      let simCover = document.createElement("div");
+      //<img src="">
+      //<img src="">
+      let frontCover = document.createElement("img");
+      frontCover.src="";
+      frontCover.classList.add("frontCover");
+      backCover.classList.add("backCover");
+      let backCover = document.createElement("img");
+      simCover.appendChild(frontCover);
+      simCover.appendChild(backCover);
+      
+      //find the number of tracks in the simulation with the given simNumber
+      //loop that number of times creating
+      //<div class="row">
+      //<img src="art/s1_2.jpg" alt="Dont fight it and you wont get hurt" width="300" height="300f">
+      //<audio controls src="audio/s1_2.mp3"></audio>
+      
+    },
+
   };
   const ready = "cordova" in window ? "deviceready" : "DOMContentLoaded";
   document.addEventListener(ready, app.init);
