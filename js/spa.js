dbfdf429c44f9c558642da332e2101679a163e72
khal0160 <khal0160@algonquinlive.com>
2020-06-05 02:07:36 -0400

    javascrript buildSimulation

--- js/spa.js
+++ js/spa.js
@@ -1,7 +1,58 @@
 
 let app = {
     pages: [],
-    
+    simulations: [
+      {
+        name: "Testing",
+        trackCount: 11, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Good Deeds And Synth",
+        trackCount: 6, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Instant Gratification",
+        trackCount: 27, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Micro dosing Fear",
+        trackCount: 9, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Alternate Immersion",
+        trackCount: 6, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Logical Eternity",
+        trackCount: 3, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "HitClips",
+        trackCount: 7, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Lasouf",
+        trackCount: 0, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Lorem Ipsum",
+        trackCount: 8, 
+        description: "Blah Blah Blah",
+      },
+      {
+        name: "Plastic 604",
+        trackCount: 10, 
+        description: "Blah Blah Blah",
+      },
+    ],
     show: new Event('show'),
     init: () => {
       console.log("after app.init")
@@ -58,30 +109,25 @@ let app = {
       history.pushState({}, currentPage, `#${currentPage}`);
       document.getElementById(hash).dispatchEvent(app.show);
     },
-    buildSimulation: (simNumber) =>{
+    buildSimulation : (simNumber) =>{
       let body = document.getElementById("body");
-
       //<section class="page">
       let simPage = document.createElement("section");
       simPage.classList.add("page");
       body.appendChild(simPage);
-
       //<div class="albumHead">Simulation_1: Testing</div>
       let simHead = document.createElement("div");
       simHead.classList.add("albumHead");
       simHead.textContent("Simulation_"+simNumber+": Testing");
       simPage.appendChild(simHead);
-
       //<div class="albumContainer">
       let simContainer = document.createElement("div");
       simContainer.classList.add("albumContainer");
       simPage.appendChild(simContainer);
-
       //<div class="row">
       let simRow = document.createElement("div");
       simRow.classList.add("row");
       simContainer.appendChild(simRow);
-
       //<div class = "cover">
       let simCover = document.createElement("div");
       //<img src="">
@@ -93,15 +139,14 @@ let app = {
       let backCover = document.createElement("img");
       simCover.appendChild(frontCover);
       simCover.appendChild(backCover);
+
       
       //find the number of tracks in the simulation with the given simNumber
       //loop that number of times creating
       //<div class="row">
       //<img src="art/s1_2.jpg" alt="Dont fight it and you wont get hurt" width="300" height="300f">
       //<audio controls src="audio/s1_2.mp3"></audio>
-      
     },
-
   };
   const ready = "cordova" in window ? "deviceready" : "DOMContentLoaded";
   document.addEventListener(ready, app.init);
