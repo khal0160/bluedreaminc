bb42c0c8f9894963ddc614d052ea15cc5372f73e
khal0160 <khal0160@algonquinlive.com>
2020-07-04 14:37:18 -0400

    material template added and root of responsive website

--- js/diveinchi.js
+++ js/diveinchi.js
@@ -7,27 +7,6 @@ let app = {
 
     },
     addListeners: ()=>{
-        // //from home to sim 1
-        // document.getElementById("sim1btn").addEventListener("click", app.nav);
-        // //from home to sim 2
-        // document.getElementById("sim2btn").addEventListener("click", app.nav); 
-        // //from home to sim 3
-        // document.getElementById("sim3btn").addEventListener("click", app.nav); 
-        // //from home to sim 4 
-        // document.getElementById("sim4btn").addEventListener("click", app.nav);
-        // //from home to sim 5 
-        // document.getElementById("sim5btn").addEventListener("click", app.nav);
-        // //from home to sim 6 
-        // document.getElementById("sim6btn").addEventListener("click", app.nav);
-        // //from home to sim 7 
-        // document.getElementById("sim7btn").addEventListener("click", app.nav);
-        // //from home to sim 8 
-        // document.getElementById("sim8btn").addEventListener("click", app.nav);
-        // //from home to sim 9 
-        // document.getElementById("sim9btn").addEventListener("click", app.nav);
-        // //from home to sim 10 
-        // document.getElementById("sim10btn").addEventListener("click", app.nav);
-
     },
     nav: ev=>{
         let btn = ev.target;
@@ -36,15 +15,22 @@ let app = {
         document.querySelector(".page.active").classList.remove("active");
         document.getElementById(target).classList.add("active");
     },
+    //buildHead
+    buildHead: ()=>{
+        console.log("after app.buildHead");
+        let body = document.getElementById("body");
+        let headTitle = document.createElement("div");
+        //headTitle.textContent="DIVEINCHI";
+        headTitle.classList.add("col", "s6","offset-s6")
+        body.appendChild(headTitle);
+
+    },
+    //buildMid
+    //buildFooter
     buildHome: ()=>{
         console.log("after app.buildHome");
-        // find the html body element
-        let body = document.getElementById("body");
         // Header
-        let header = document.createElement("div");
-        header.id="header";
-        header.textContent="DIVEINCHI";
-        body.appendChild(header);
+        app.buildHead();
         // Middle
         let middle = document.createElement("div");
         middle.id="middle";
