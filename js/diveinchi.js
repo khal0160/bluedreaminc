3115c9b1aa2a7c87afa3a5483d866d6cfd3a92d8
khal0160 <khal0160@algonquinlive.com>
2020-07-07 10:07:09 -0400

    clean code with basic layout

--- js/diveinchi.js
+++ js/diveinchi.js
@@ -1,49 +1,29 @@
 let app = {
+    pages: [],
+    show: new Event('show'),
     init: ()=>{
         console.log("after app.init")
-        //add click listeners for navigation
-        app.buildHome();
         app.addListeners();
-
+        app.pages = document.querySelectorAll('.page');
+        app.pages.forEach((pg)=>{
+            pg.addEventListener('show', app.pageShown);
+        })
+        document.querySelectorAll('.simButton').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
     },
     addListeners: ()=>{
             var elems = document.querySelectorAll('.dropdown-trigger');
             var instances = M.Dropdown.init(elems, []);
     },
     nav: ev=>{
+        ev.preventDefault();
         let btn = ev.target;
         let target = btn.getAttribute("data-target");
         console.log("Navigate to", target);
         document.querySelector(".page.active").classList.remove("active");
         document.getElementById(target).classList.add("active");
     },
-    //buildHead
-    buildHead: ()=>{
-        console.log("after app.buildHead");
-        let body = document.getElementById("body");
-        let headTitle = document.createElement("div");
-        //headTitle.textContent="DIVEINCHI";
-        headTitle.classList.add("col", "s6","offset-s6")
-        body.appendChild(headTitle);
-
-    },
-    //buildMid
-    //buildFooter
-    buildHome: ()=>{
-        console.log("after app.buildHome");
-        // Header
-        app.buildHead();
-        // Middle
-        let middle = document.createElement("div");
-        middle.id="middle";
-        body.appendChild(middle);
-        // Footer
-        let footer = document.createElement("div");
-        footer.id="footer";
-        body.appendChild(footer);
-    }
-
-
 }
 const ready = "cordova" in window ? "deviceready" : "DOMContentLoaded";
 document.addEventListener(ready, app.init);
